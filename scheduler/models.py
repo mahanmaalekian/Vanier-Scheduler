327c2638ff5fa6d00c2397ffb0fa32ffc6f4ed5d
Mahan Maalekian <maalekianmahan@gmail.com>
2024-10-18 15:32:42 -0400

    started search page

--- scheduler/models.py
+++ scheduler/models.py
@@ -7,7 +7,7 @@ class Exam(models.Model):
     date = models.DateField()
     course_id = models.CharField(
         max_length=8,
-        validators=[MinLengthValidator(10), MaxLengthValidator(10)]
+        validators=[MinLengthValidator(8), MaxLengthValidator(8)]
     )
     section_id = models.CharField(max_length=10)
     description = models.CharField(max_length=64)
@@ -15,7 +15,7 @@ class Exam(models.Model):
     start_time = models.TimeField()
     end_time = models.TimeField()
     room_number = models.CharField(max_length=10)
-    row=models.CharField(max_length=10, null=True)
+    row=models.CharField(max_length=10, null=True, blank=True)
 
     def __str__(self):
         return f"{self.description}: sections {self.section_id}"
--- scheduler/views.py
+++ scheduler/views.py
@@ -1,7 +1,28 @@
+import json
 from django.shortcuts import render
 from django.http import HttpResponse
+from .models import *
 
  # Create your views here.
 
+searched_courses = []
+
 def index(request):
-     return HttpResponse("Hello, world!")
+     return render(request, "scheduler/index.html")
+
+def search(request):
+     query = request.POST.get('q')
+     exams = []
+     courses = Exam.objects.values_list('course_id', flat=True)
+     if query:
+          query = query.upper()
+          if query in courses:
+               exam = Exam.objects.filter(course_id = query)
+               exams.append(exam)
+          else:
+               exams = Exam.objects.filter(course_id__icontains=query)
+     print(exams)
+     return render(request, "scheduler/search.html", {
+          "exams": exams
+     })
+
